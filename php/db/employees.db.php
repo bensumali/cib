16c86144f230a06fed3513773e315925c622e941
Ben Sumali <bsumali@uci.edu>
2016-06-16 23:39:21 -0700

    Implemented new table to hold employee statuses

--- php/db/employees.db.php
+++ php/db/employees.db.php
@@ -12,12 +12,18 @@ class EmployeesDb
         $result = array();
         try {
           $q = "
-            SELECT e.name_first, e.name_middle, e.name_last, e.ucinetid, e.email, e.id, e.type, d.department
+            SELECT e.name_first, e.name_middle, e.name_last, e.ucinetid, e.email, e.id, e.type, d.department,
+              s.status, s.estimated_return, s.location
             FROM employee AS e
             JOIN employee_of_department AS d ON e.ucinetid = d.ucinetid
+            LEFT JOIN employee_status AS s ON s.ucinetid = e.ucinetid
             WHERE d.department = :department
             AND e.removed_by IS NULL
+            AND e.current = 1
             AND d.removed_by IS NULL
+            AND s.current = 1
+            AND s.removed_by IS NULL
+            ORDER BY s.created_when DESC
           ";
           $eq = $this->pdo->connection->prepare($q);
           $eq->bindParam(":department", $department, PDO::PARAM_STR);
